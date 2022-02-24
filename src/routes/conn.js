117515b084d4970e23521345e9b7543799cd023e
Nandini <nandinituptewar@gmail.com>
2022-02-24 22:29:22 +0530

    refresh token added

--- src/routes/conn.js
+++ src/routes/conn.js
@@ -8,6 +8,8 @@ const connection= require('./db');
 const router = express.Router();
 const { checkToken } = require("../auth/token_validation");
 
+let refreshTokens = {};
+
 router.use(express.json());
 
 //Display the records
@@ -20,7 +22,7 @@ router.get("/",checkToken,(req,res) => {
 });
 
 //insert the record
-router.post('/adding_data', function (req, res) {
+router.post('/adding_data',checkToken, function (req, res) {
     var postData  = req.body;
     connection.query('INSERT INTO employeetable SET ?', postData, function (error, results, fields) {
        if (error) throw error;
@@ -30,7 +32,7 @@ router.post('/adding_data', function (req, res) {
  });    
 
 //delete records
- router.delete('/deleting_data/:id', function (req, res) {
+ router.delete('/deleting_data/:id',checkToken, function (req, res) {
     connection.query('DELETE FROM employeetable WHERE EmpID=?', [req.params.id], function (error, results, fields) {
        if (error) throw error;
        res.send('Record has been deleted!');
@@ -39,7 +41,7 @@ router.post('/adding_data', function (req, res) {
  });
 
 //Update records
- router.put('/updating_data', function (req, res) {
+ router.put('/updating_data', checkToken, function (req, res) {
     connection.query('UPDATE employeetable SET LastName=?, FirstName=?,Gender=?,Age=?,Contact=?,Email=? where EmpID=?', [req.body.LastName,req.body.FirstName, req.body.Gender, req.body.Age,req.body.Contact,req.body.Email,req.body.EmpID], function (error, results, fields) {
        if (error) throw error;
        res.send(JSON.stringify(results));
@@ -49,7 +51,7 @@ router.post('/adding_data', function (req, res) {
 
  //login
  router.post('/login_data', function (req, res) {
-    var postData  = req.body;
+    const postData  = req.body;
     console.log('1');
     connection.query('select * from userstable WHERE UserID= ?', [req.body.UserID] , function (error, results, fields) {
        if (error) {
@@ -58,11 +60,14 @@ router.post('/adding_data', function (req, res) {
        else{
            if(req.body.UserName==results[0]["UserName"]){
             results[0]["UserID"]= undefined; 
-            const jsontoken= sign({result:results[0]},"abz134", {expiresIn: "1h"} ) ;
+            const jsontoken= sign({result:results[0]},"abz134", {expiresIn: "1m"} ) ;
+            const refreshtoken= sign({result:results[0]},"pqr134", {expiresIn: "1d"} ) ;
+            refreshTokens[refreshtoken]=results[0];
             return res.json({
                 success: 1,
                 message: "login successfully",
-                token: jsontoken
+                token: jsontoken,
+                refresh: refreshtoken 
               });
            }
            else{
@@ -71,5 +76,31 @@ router.post('/adding_data', function (req, res) {
        }
      });
  });
+
+router.post("/refresh_token", (req, res, next) => {
+
+    const refreshToken = req.body.token;
+    if (!refreshToken || !refreshTokens[refreshToken]) {
+        return res.json({ message: "Refresh token n4ot found, login again" });
+    }
+    // If the refresh token is valid, create a new accessToken and return it.
+    jwt.verify(refreshToken, "pqr134", (err, user) => {
+        console.log(user);
+        if (!err) {
+            let user_data= refreshTokens[refreshToken]
+            const accessToken = jwt.sign(user_data, "abz134", {
+                expiresIn: "1m"
+            });
+            return res.json({ success: true, accessToken });
+        } else {
+            return res.json({
+                success: false,
+                message: "Invalid refresh token"
+            });
+        }
+    });
+});
+
+
 
 module.exports=router;
